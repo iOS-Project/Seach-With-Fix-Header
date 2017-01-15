f3bc27eb7a3410f9fd92dc7b167c7d49b2160cca
= <=>
2017-01-15 09:59:31 +0700

    first commit

--- Search/HeaderView.swift
+++ Search/HeaderView.swift
@@ -9,13 +9,23 @@
 import UIKit
 
 class HeaderView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        //setupView()
+    }
+    
+    func setupView() {
+        print("awake")
+        let v  = Bundle.main.loadNibNamed("HeaderView", owner: self, options: nil)?.first as! UIView
+        self.addSubview(v)
+        v.frame = self.bounds
+    }
 }
--- Search/TableViewController.swift
+++ Search/TableViewController.swift
@@ -8,88 +8,124 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+let searchCell = "SearchCell"
+let optionCell = "OptionCell"
+let segmentCell = "SegmentCell"
+let itemCell = "ItemCell"
 
+class TableViewController: UITableViewController {
+    
+    var searchButton : UIBarButtonItem?
+    var vHeader : UIView? = nil
+    var searchBar : UISearchController? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        tableView.register(UINib(nibName: searchCell, bundle: nil), forCellReuseIdentifier: searchCell)
+        
+        tableView.register(UINib(nibName: optionCell, bundle: nil), forCellReuseIdentifier: optionCell)
+        
+        tableView.register(UINib(nibName: segmentCell, bundle: nil), forCellReuseIdentifier: segmentCell)
+        
+        tableView.register(UINib(nibName: itemCell, bundle: nil), forCellReuseIdentifier: itemCell)
+        
+        tableView.bounces = false
+        
+        addSearchButton()
+        addSegment()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addSegment(){
+        vHeader = HeaderView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: 50))
     }
-
-    // MARK: - Table view data source
-
+    
+    func addSearchButton(){
+        searchButton = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(searchButtonClick(_:)))
+        
+        searchBar = UISearchController(searchResultsController: nil)
+        searchBar?.searchBar.sizeToFit()
+        searchBar?.hidesNavigationBarDuringPresentation = false
+    }
+    
+    func toggleSearchButton(_ show:Bool){
+        if show{
+            navigationItem.rightBarButtonItem = searchButton
+        }else{
+            navigationItem.rightBarButtonItem = nil
+        }
+        
+        navigationItem.titleView = nil
+    }
+    
+    func searchButtonClick(_ sender:UIBarButtonItem){
+        
+        self.navigationItem.titleView = searchBar?.searchBar
+        navigationItem.rightBarButtonItem = nil
+    }
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let rect = tableView.rectForRow(at: IndexPath(row: 0, section: 0))
+        if scrollView.contentOffset.y > rect.origin.y{
+            toggleSearchButton(true)
+        }else{
+            toggleSearchButton(false)
+        }
+        
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        switch section{
+        case 0:
+            return 2
+        case 1:
+            return 50
+        default:
+            return 0
+        }
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        
+        if indexPath.section == 0 && indexPath.row == 0{
+            let cell = tableView.dequeueReusableCell(withIdentifier: optionCell, for: indexPath) as! OptionCell
+            cell.sizeToFit()
+            return cell
+        }else if indexPath.section == 0 && indexPath.row == 1{
+            let cell = tableView.dequeueReusableCell(withIdentifier: searchCell, for: indexPath) as! SearchCell
+            cell.sizeToFit()
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: itemCell, for: indexPath) as! ItemCell
+            return cell
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Search"
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch section{
+        case 1:
+            return 50
+        default:
+            return 0
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        switch section{
+        case 1:
+            return vHeader
+        default:
+            return nil
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
